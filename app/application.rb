0b3785325018383d95523f2c538cc2e9f69a90cb
ccchippp <clempke90@gmail.com>
2021-02-01 12:03:26 -0500

    very interesting'

---
+++ app/application.rb
@@ -0,0 +1,38 @@
+# class Application
+
+#     def call(env)
+#       resp = Rack::Response.new
+#       req = Rack::Request.new(env)
+  
+#       if req.path=="/songs"
+#         resp.write "You requested the songs"
+#       else
+#         resp.write "Route not found"
+#         resp.status = 404
+#       end
+  
+#       resp.finish
+#     end
+class Application
+    @@items = [Item.new("Crayons", 3.42)]
+    
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last
+            if item = @@items.find{|i| i.name == item_name}
+                resp.write item.price
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+
+end
